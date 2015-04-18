ac338774f49f7c931783ede9023d666b4c1a3160
Ramon Eelman <ramon@ramoneelman.org>
2015-04-18 21:12:09 +0200

    Working tcp connection to our server, prints a lot of japanese characters in a TextArea, some amazing network programming right there.

---
+++ Reinier Sanders2/src/main/Log.java
@@ -0,0 +1,71 @@
+package main;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Log {
+	private DataInputStream in;
+	private DataOutputStream out;
+	private ServerSocket serv;
+	
+	public Log()
+	{
+		establishConnection();
+		out("test\n");
+		keepSending();
+	}
+	
+	private synchronized void establishConnection() {
+		try {
+			serv = new ServerSocket(1337);
+		} catch (IOException e) {
+			
+		}
+		while (true)
+			try {
+				Socket s = serv.accept();
+				in = new DataInputStream(s.getInputStream());
+				out = new DataOutputStream(s.getOutputStream());
+				return;
+			} catch (IOException e) {
+				// LCD.drawString("Failed to establish network connection trying again",
+				// 0, 0);
+				System.out.print("Failed to establish network connection");
+				try {
+					wait(2000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
+		
+	}
+	
+	public void out(String output) {
+		try {
+			out.writeUTF(output + "\n");
+			out.flush();
+		} catch (IOException e) {
+			// LCD.drawString("Failed to output string: trying to reconnect",
+			// 0, 0);
+			System.out.println("Failed to output string: trying to reconnect");
+			
+			establishConnection();
+		}
+		
+	}
+	
+	public synchronized void keepSending() {
+		while (true) {
+			out("見る");
+			try {
+				wait(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- Reinier Sanders2/src/main/Main.java
+++ Reinier Sanders2/src/main/Main.java
@@ -1,35 +1,10 @@
 package main;
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.motor.Motor;
-
 
 public class Main {
-
-	public static void main(String[] args) {
-		Motor.B.setSpeed(720);// 2 RPM
-		   Motor.C.setSpeed(720);
-		 
-		   
-		   Motor.B.forward();
-		   Motor.C.forward();
-		   try {
-			Thread.sleep (500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		   Motor.B.stop();
-		  
-		   Motor.C.stop();
-		   Motor.B.rotateTo( 360);
-		   Motor.B.rotate(-720,true);
-		   while(Motor.B.isMoving() )
-				   Thread.yield();
-		   int angle = Motor.B.getTachoCount(); // should be -360
-		   LCD.drawInt(angle,0,0);
-		 
-	}
-
+	static Log l;
 	
-
+	public static void main(String[] args) {
+		l = new Log();
+	}
+	
 }
